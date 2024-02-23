a6f19829906a7bb0f5a191a5e7a4679b32bcc559
KarinaBhaldar <138844356+KarinaBhaldar@users.noreply.github.com>
2024-02-23 11:48:54 +0530

    dashboard

---
+++ karinaBhaldar_20/20-2-2024/hoisting.js
@@ -0,0 +1,18 @@
+greet()
+ function greet(){
+  console.log("Hello Good Morning")
+ }
+
+
+ kareena()
+   function kareena(){
+    console.log("Heyyy")
+   }
+ 
+
+   var x=7;
+   function getname(){
+    console.log(x)
+
+   }
+   
